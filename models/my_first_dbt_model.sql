3d3534c8d2f25b2eea7717a3bd15606d45765ea1
aparicio.jv@gmail.com <aparicio.jv@gmail.com>
2023-04-24 17:05:27 +0000

    test models

--- models/my_first_dbt_model.sql
+++ models/my_first_dbt_model.sql
@@ -7,21 +7,14 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
+with top100 as 
+(
+  select *
+  from {{ source('staging','test') }}
 )
 
 select *
-from source_data
+from top100
 
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
